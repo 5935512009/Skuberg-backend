d9b9a6123b219aea2dfec92089079020e5baff09
5935512009 <Pasut2541@hotmail.com>
2025-03-27 22:55:52 +0700

    update:{import path and close FE}

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 const express = require("express");
-const path = require("path");
-
+// const path = require("path");
+const userRoutes = require("./routes/userRoutes");
+const walletRoutes = require("./routes/walletRoutes");
+const cryptoRoutes = require("./routes/cryptoRoutes");
+const transactions = require("./routes/transactionRoutes");
 const app = express();
+app.use(express.json());
 
-// Middleware สำหรับเสิร์ฟไฟล์ static (เช่น CSS, รูปภาพ)
-app.use(express.static(path.join(__dirname, "views")));
-app.use(express.static(path.join(__dirname, "views/crypto_coin")));  // ต้องให้ path ตรงกับโฟลเดอร์จริง
+
+// // Middleware สำหรับเสิร์ฟไฟล์ static (เช่น CSS, รูปภาพ)
+// app.use(express.static(path.join(__dirname, "views")));
+// app.use(express.static(path.join(__dirname, "views/crypto_coin")));  // ต้องให้ path ตรงกับโฟลเดอร์จริง
 
 // ทดสอบ API
 app.get('/test', (req, res) => {
     res.json("test hello world");
 });
 
-// เสิร์ฟหน้า index.html
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "views", "index.html"));
-});
+app.use("/users", userRoutes);
+app.use("/wallets",walletRoutes);
+app.use("/cryptos",cryptoRoutes);
+app.use("/transactions",transactions);
+// // เสิร์ฟหน้า index.html
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "views", "index.html"));
+// });
 
-// เสิร์ฟหน้า crypto.html
-app.get("/crypto", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/crypto_coin", "Crypto.html"));
-});
+// // เสิร์ฟหน้า crypto.html
+// app.get("/crypto", (req, res) => {
+//     res.sendFile(path.join(__dirname, "views/crypto_coin", "Crypto.html"));
+// });
 
 // Start server
 const PORT = 4001;
