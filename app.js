2604c31a2463ca5850807d2662512c64dbfb0bd3
5935512009 <Pasut2541@hotmail.com>
2025-03-27 09:52:00 +0700

    update:{basic html css }

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const path = require("path")
+
 const app = express();
 
-//Routes
-app.get('/test',async(requestAnimationFrame,res)=>{
+//tes path
+app.get('/test',async(req,res)=>{
     res.json("test hello world");
 });
 
+// path 
+app.use(express.static(path.join(__dirname, "views")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
 
 const PORT = 4001;
 app.listen(PORT, ()=>{
-    console.log(`server is running on ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 })
