c0e13c4f088092a98fb4c62c2c09173856b40a10
5935512009 <Pasut2541@hotmail.com>
2025-03-28 18:31:20 +0700

    update:{post transaction}

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -12,3 +12,47 @@ exports.getAllTransactions = (req,res) =>{
     const transactions = readTransactions();
     res.json(transactions);
 }
+
+exports.createTransaction = (req,res) =>{
+    const transactions = readTransactions();
+    const wallets = JSON.parse(fs.readFileSync(__dirname,"../data/wallets.json"));
+    const cryptos = JSON.parse(fs.readFileSync(__dirname,"../data/wallets.json"));
+    
+    const {user_id,wallet_id,crypto_id,type,amount} = req.body;
+    
+    // check ว่า wallet และ crypto มีค่าอยู่จริงใหม
+    const wallet = wallets.find(w => w.wallet_id ===  wallet_id && w.user_id === user_id);
+    const crypto = cryptos.find(crypto_id.crypto_id === crypto_id);
+
+    if(!wallet || !crypto){
+        return res.stauts(400).json({error:"Invaild wallet and crypto"})
+    }
+
+    // คำนวนราคาซื้อขาย
+    const price_per_coin = crypto.price;
+    const total_price = price_per_coin * amount; // ราคาที่เราจ่ายซื้อ
+
+    if(type === "BUY" ){
+        wallet.balance -= total_price;
+    }else if(type === "SELL"){
+        wallet.balance += total_price;
+    }else{
+        return res.stauts(400).json({error: "Invaild transaction type"});
+    }
+
+    const newTransactions = {
+        transaction_id: length +1,
+        user_id,
+        wallet_id,
+        type,
+        amount,
+        price_per_coin,
+        total_price,
+        date:new Date().toDateString()
+
+    };
+    transactions.push(newTransactions);
+    fs.writeFileSync(TransactionsFilePath, JSON.stringify(transactions,null,2)); //create transaction details
+    fs.writeFileSync(path.join(__dirname,"../data/wallets.json"), JSON.stringify(wallets,null,2)); //update wallets balance
+    res.stauts(201).json(transactions);
+};
--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const {getAllTransactions} = require("../controllers/transactionController");
+const {getAllTransactions,newTransactions} = require("../controllers/transactionController");
 
 const router = express.Router();
 
 router.get("/",getAllTransactions);
-
+router.post("/",newTransactions);
 module.exports = router;
